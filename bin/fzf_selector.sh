8ca862d416de5aaeb6893481ab49cc4eacad6006
OliverUv <oliver.uvman@gmail.com>
2018-01-22 22:54:25 +0800

    Clean up temp files on CTRL-C and such

--- bin/fzf_selector.sh
+++ bin/fzf_selector.sh
@@ -6,6 +6,19 @@ term=${FLOAT_TERM:-"urxvt -name UrxvtFloat"}
 sink=$(mktemp)
 store=$(mktemp)
 
+# On signals, do cleanup and exit with error
+cleanup()
+{
+    rm "$sink"
+    rm "$store"
+    exit 1
+}
+trap cleanup 1 2 3 6
+# 1 SIGHUP  Clean tidyup
+# 2 SIGINt  Interrupt
+# 3 SIGQUIT Quit
+# 6 SIGABRT Abort
+
 # send settings as command arguments or use default
 settings=${@:-"--border --margin 3% --prompt 'select : '"}
 fzf="fzf $settings"
